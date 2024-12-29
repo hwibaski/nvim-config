10f2ba8c31d2e3b6391a01fc00f077edced9bb7b
hwibaski <hwibaski@gmail.com>
2024-12-29 19:28:09 +0900

    set keymap for execute lua in editor

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,3 +44,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- [[ For Execute Lua ]]
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
