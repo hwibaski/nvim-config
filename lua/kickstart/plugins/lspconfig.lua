a8adb5544ea931abcf495355cdba2bffc68eea1a
hwibaski <hwibaski@gmail.com>
2024-12-30 10:14:48 +0900

    typescript orgnize import 적용

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -109,7 +109,6 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
@@ -119,10 +118,10 @@ return {
           -- ts_ls 일 때만 실행
           if client and client.name == 'ts_ls' then
             map('<leader>oi', function()
-              vim.lsp.buf.execute_command({
-                command = "_typescript.organizeImports",
-                arguments = {vim.api.nvim_buf_get_name(0)},
-              })
+              vim.lsp.buf.execute_command {
+                command = '_typescript.organizeImports',
+                arguments = { vim.api.nvim_buf_get_name(0) },
+              }
             end, '[O]rganize [I]mports')
           end
 
@@ -201,22 +200,23 @@ return {
         ts_ls = {
           settings = {
             typescript = {
+              updateImportsOnFileMove = { enabled = 'always' },
               preferences = {
-                importModuleSpecifier = "non-relative",  -- "relative" 또는 "non-relative"
-              }
-            }
+                importModuleSpecifier = 'non-relative', -- "relative" 또는 "non-relative"
+              },
+            },
           },
           commands = {
             OrganizeImports = {
               function()
-                vim.lsp.buf.execute_command({
-                  command = "_typescript.organizeImports",
-                  arguments = {vim.api.nvim_buf_get_name(0)},
-                })
+                vim.lsp.buf.execute_command {
+                  command = '_typescript.organizeImports',
+                  arguments = { vim.api.nvim_buf_get_name(0) },
+                }
               end,
-              description = "Organize Imports"
-            }
-          }
+              description = 'Organize Imports',
+            },
+          },
         },
         lua_ls = {
           -- cmd = { ... },
