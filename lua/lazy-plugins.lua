3f120965440e19b2c3cc0673cd85479f330c218e
hwibaski <hwibaski@gmail.com>
2024-12-30 10:14:59 +0900

    install oil.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -59,13 +59,13 @@ return {
 
     require 'kickstart.plugins.git-blame',
 
+    require 'kickstart.plugins.oil',
+
     -- require 'kickstart.plugins.lint',
     -- require 'kickstart.plugins.nvim-tree',
-
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- place them in the correct locations.
-
     -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
     --
     --  Here are some example plugins that I've included in the Kickstart repository.
@@ -77,7 +77,6 @@ return {
     -- require 'kickstart.plugins.autopairs',
     -- require 'kickstart.plugins.neo-tree',
     -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
